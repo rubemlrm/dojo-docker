6cd1d251a3725cf3014e055b6bbabfa8ba0731b3
Rubem <rubemmota89@gmail.com>
2023-06-08 21:24:57 +0100

    refactor(services):  change variables handling
    Added roles to setup dnf, firewall, openssh, user configs
    Added settings for irc,homeassistant,media services.
    
    Signed-off-by: Rubem <rubemmota89@gmail.com>

---
+++ roles/users/files/zsh/.config/zsh/alias.zsh
@@ -0,0 +1,5 @@
+source "$ZSH_CUSTOM/alias/ansible.zsh"
+source "$ZSH_CUSTOM/alias/kubernetes.zsh"
+source "$ZSH_CUSTOM/alias/networking.zsh"
+source "$ZSH_CUSTOM/alias/python.zsh"
+source "$ZSH_CUSTOM/alias/utils.zsh"
