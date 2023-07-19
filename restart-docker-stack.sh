0b6e3671ed961adf2ccb54bc4d2c60890f968864
Rubem Mota <rubemmota89@gmail.com>
2023-07-19 19:44:42 +0100

    refactor(firewall): Changed definition of firewall ports
    Changed definition of firewall ports to be by service instead global
    Changed default location of docker files to use ssd disk
    Fixed wrong docker stack version on stacks
    
    Signed-off-by: Rubem Mota <rubemmota89@gmail.com>

---
+++ restart-docker-stack.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Update/rebalance services in docker swarm cluster. Run this script after docker daemon restart
+
+echo "*** Updating Docker Swarm Services ***"
+for x in `docker service ls | awk '{print $2}' | tail -n+2`;
+do
+  echo -n "-> "
+  docker service update  --force $x -q
+  sleep 10
+done
