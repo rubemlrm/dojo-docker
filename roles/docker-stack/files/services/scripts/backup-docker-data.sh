4891d5374663da4090ae1759998069f4ce23395e
Rubem <rubemmota89@gmail.com>
2023-08-06 16:20:10 +0100

    fix(homeassistant): Fixed homeassistant configuration
    Fixed homeassistant configuration
    Changed restart rule to restart on every fail
    
    Signed-off-by: Rubem <rubemmota89@gmail.com>

---
+++ roles/docker-stack/files/services/scripts/backup-docker-data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+tar -zcvf /home/rubemlrm/docker/data/docker-backups/volumes/$(date +%s).tar.gz \
+ --exclude="**/cache/**" \
+ /home/rubemlrm/docker/data/volumes
+
+chown -R rubemlrm:rubemlrm /home/rubemlrm/docker/data/docker-backups/
