5ad27f61d333e65d4a9b9280e09802ff13e9dfda
Rubem <rubemmota89@gmail.com>
2023-11-04 17:53:05 +0000

    refactor(env): Changed loading of env vars to use .env files
    Signed-off-by: Rubem <rubemmota89@gmail.com>

--- scripts/backup-docker-data.sh
+++ scripts/backup-docker-data.sh
@@ -1,7 +1,12 @@
-w#!/bin/bash
+#!/bin/bash
 
 tar -zcvf /home/rubemlrm/docker/data/docker-backups/volumes/$(date +%s).tar.gz \
  --exclude="**/cache/**" \
+ --exclude="/home/rubemlrm/docker/data/volumes/media/config/transcodes/**" \
+ --exclude="/home/rubemlrm/docker/data/volumes/media/rutorrent/**" \
+ --exclude="/home/rubemlrm/docker/data/volumes/torrents/**" \
+ --exclude="/home/rubemlrm/docker/data/volumes/kavita/covers" \
+ --exclude="/home/rubemlrm/docker/data/volumes/media/config/metadata/**" \
  /home/rubemlrm/docker/data/volumes
 
 chown -R rubemlrm:rubemlrm /home/rubemlrm/docker/data/docker-backups/
