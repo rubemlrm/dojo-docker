eaac88c053ebf939ac86a65f2664c14dd33c3b6d
Rubem <rubemmota89@gmail.com>
2024-01-25 19:57:11 +0000

    fix(linting): Fixing ansible and shell linter errors
    Signed-off-by: Rubem <rubemmota89@gmail.com>

--- scripts/reconnect-to-wifi.sh
+++ scripts/reconnect-to-wifi.sh
@@ -1,7 +1,4 @@
-# WiFi connectivity test (and reconnect if needed)
-# Source: https://feriman.com/automatically-reconnect-to-wifi-on-raspberry-pi/
-#
-# Check the connectivity
+#!/bin/bash
 if ! ping -c2 8.8.8.8 > /dev/null; then
     ifconfig wlan0 down
     sleep 5
